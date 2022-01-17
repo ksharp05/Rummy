b45bdd060d63327b58fc9ea5fc387670eb697a84
ksharp05 <yadavlakshya05@gmail.com>
2022-01-17 16:15:06 +0530

    POC

---
+++ Assets/rummy/Scripts/CardSpotsNode.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using rummy.Cards;
+using UnityEngine;
+
+namespace rummy
+{
+
+    public class CardSpotsNode : RadialLayout<CardSpot>
+    {
+        public GameObject CardSpotPrefab;
+
+        public float degreesPerSpot = 30;
+
+        public float spotStartAngle;
+        public float spotRadius = 3f;
+        public float spotAngleSpread = 180f;
+
+        public override List<CardSpot> Objects { get; protected set; } = new List<CardSpot>();
+        protected override void InitValues() { }
+
+        public CardSpot AddCardSpot()
+        {
+            GameObject cardSpotGO = Instantiate(CardSpotPrefab, transform.position, Quaternion.identity, transform);
+            cardSpotGO.name = transform.parent.name + "-CardSpot" + (Objects.Count + 1);
+            CardSpot cardSpot = cardSpotGO.GetComponent<CardSpot>();
+            cardSpot.startAngle = spotStartAngle;
+            cardSpot.radius = spotRadius;
+            cardSpot.angleSpread = spotAngleSpread;
+
+            Objects.Add(cardSpot);
+            angleSpread = Objects.Count * degreesPerSpot;
+            UpdatePositions();
+            Objects.ForEach(spot => spot.UpdatePositions());
+            return cardSpot;
+        }
+
+        public List<Card> ResetNode()
+        {
+            var cards = new List<Card>();
+            foreach (var cardSpot in Objects)
+                cards.AddRange(cardSpot.ResetSpot());
+
+            while (Objects.Count > 0)
+            {
+                CardSpot obj = Objects[0];
+                Objects.RemoveAt(0);
+                Destroy(obj.gameObject);
+            }
+            return cards;
+        }
+
+
+    }
+
+}
---
+++ Assets/rummy/Scripts/UI/Options/ApplyNewSeed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using rummy.Utility;
+
+namespace rummy.UI.Options
+{
+
+    public class ApplyNewSeed : MonoBehaviour
+    {
+        public InputField NewSeedInput;
+        public void ApplySeed()
+        {
+            int.TryParse(NewSeedInput.text, out int newSeed);
+            Tb.I.GameMaster.Seed = newSeed;
+        }
+    }
+
+}
